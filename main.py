acc16c633230a72735cfa8256969ec82f8eb031c
Retardo <loginrevoked@gmail.com>
2026-02-07 15:13:31 +0300

    simple test server

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.post("/report")
 async def respond_to_report(request:req):
-   json_ = await req.json()
+   json_ = await request.json()
    print(json_)
    return JSONResponse({"success":True})
 
